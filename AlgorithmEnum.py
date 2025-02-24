cba45e4737c35cca597e058870c9912504d90e07
Dan Jackson <danieljackson634@gmail.com>
2025-02-24 16:00:32 +0000

    final commit

--- AlgorithmEnum.py
+++ AlgorithmEnum.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+"""
+Used to specify which algorithm to use
+"""
 class Algorithm(Enum):
     SCAN = 1
     LOOK = 2
--- Call.py
+++ Call.py
@@ -1,4 +1,7 @@
+"""
+Request object used by LiftManager
+"""
 class Call:
     def __init__(self, requested_floor : int, isInternal : bool):
-        self.requested_floor = requested_floor
-        self.isInternal = isInternal
+        self.requested_floor : int = requested_floor
+        self.isInternal : bool = isInternal
